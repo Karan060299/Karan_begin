f1a87fc39272e1e0ff6580f25e82ebe4fb54b794
Karan060299 <apoorva.karan1@gmail.com>
2025-06-16 23:47:05 +0530

    save

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,3 +1,22 @@
-console.log("Karan")
-const age = 20
-console.log(age)
+// console.log("Karan")
+// const age = 20
+// console.log(age)
+
+// console.log("Zuber")
+// let Age = 23
+// console.log(Age)
+
+//do not repeat variable declaration
+
+let name
+let age
+
+name="Karan"
+age=20
+console.log(name,age)
+
+
+
+name="Ajay"
+age=16
+console.log(name,age)
